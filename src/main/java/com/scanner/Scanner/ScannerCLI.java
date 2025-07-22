10bf2a90e7f66096f86ad658485a85921e52f4c5
sabin <sabinkarki064@gmail.com>
2025-07-22 15:28:20 +0545

    supports yaml dependency

--- src/main/java/com/scanner/Scanner/ScannerCLI.java
+++ src/main/java/com/scanner/Scanner/ScannerCLI.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,11 +18,13 @@ public class ScannerCLI implements CommandLineRunner {
     private final ApplicationArguments arguments;
     private final PropertiesFileParser propertiesFileParser;
     List<SecurityRule> securityRules;
+    private final YamlFileParser yamlFileParser;
 
-    public ScannerCLI(ApplicationArguments arguments,PropertiesFileParser propertiesFileParser ,List<SecurityRule> securityRules) {
+    public ScannerCLI(ApplicationArguments arguments,PropertiesFileParser propertiesFileParser ,List<SecurityRule> securityRules,YamlFileParser yamlFileParser) {
         this.arguments = arguments;
         this.propertiesFileParser=propertiesFileParser;
         this.securityRules=securityRules;
+        this.yamlFileParser=yamlFileParser;
     }
 
     @Override
@@ -49,7 +49,12 @@ public class ScannerCLI implements CommandLineRunner {
                     //at the end use collector to terminate and append all in list !
                     List<Path> propertiesFiles = Files.walk(currentPath)
                             .filter(Files::isRegularFile)
-                            .filter(path->path.getFileName().toString().equals("application.properties"))
+                            .filter(path->{
+                               String fileName = path.getFileName().toString();
+                               return fileName.equals("application.properties")
+                                       || fileName.equals("application.yml")
+                                        || fileName.equals("application.yaml");
+                            }) // using 1 filter to implement multiple logical OR
                             .collect(Collectors.toList());
                     if(propertiesFiles.isEmpty()){
                         System.err.println("No application file found in the project");
@@ -58,22 +63,33 @@ public class ScannerCLI implements CommandLineRunner {
                         for(Path file : propertiesFiles){
                             System.out.println(" ->  Found  file : " + file);
                             try {
-                                Properties parsedProperties= this.propertiesFileParser.parse(file);
-                                // this i believe will now parse the file whifh stores the properties file , , although i am a little confused about this line...like why did i intilaise parsedProperties variable with Properties class gaain...and the right side whole this.propertiesfileparser.parse(),i knwo this is calling method byt why use of this and why not just propertiesfileeparser.parse()..
-                                System.out.println(" -> parsing the file " + file.getFileName());
+                                // Okay so we will need a variable to store the flattened config now of type map interface
+                                Map<String,String> flatConfig = new HashMap<>();
+                                String fileName = file.getFileName().toString();
+                                if(fileName.endsWith(".properties")){
+                                    // okay inside the block ..now what..logically...now i know .if my file ends with .properties...
+                                    Properties parsedProperties = this.propertiesFileParser.parse(file);
+
+                                    // now loop through all the key in properties object and get the key but only the string ,since thats what will be in the final format
+                                    for(String key : parsedProperties.stringPropertyNames()){
+                                        flatConfig.put(key,parsedProperties.getProperty(key)); // the latter method here ofcourse is use to give value of the given key
+                                    }
+                                }else if(fileName.endsWith(".yml") || file.endsWith(".yaml")){
+                                 flatConfig = this.yamlFileParser.parse(file);
+                                }
 
                                 List<String> vulnerablities = new ArrayList<>(); // this lsit is created to hold content/vulnerablitie sof the file we jsut parsed a step above
                                 for(SecurityRule rule : this.securityRules){
-                                    String result = rule.execute(parsedProperties);
+                                    String result = rule.execute(flatConfig);
                                     if(result!=null){
                                         vulnerablities.add(result);
                                     }
                                 }
                                 //now after checking the rule
                                 if(vulnerablities.isEmpty()){
-                                    System.out.println(" [PASS] NO VULNERABLITIES DETECTED"+file.getFileName());
+                                    System.out.println(" [PASS] NO VULNERABLITIES DETECTED : "+file.getFileName());
                                 }else{
-                                    System.err.println(" [FAIL] VULNERABILTIIES DETECTED " + file.getFileName());
+                                    System.err.println(" [FAIL] VULNERABILTIIES DETECTED :  " + file.getFileName());
                                     for(String vulnerability : vulnerablities){
                                         System.err.println("  " + vulnerability);
                                     }
@@ -81,6 +97,7 @@ public class ScannerCLI implements CommandLineRunner {
                             }catch(IOException e){
                                 System.out.println("Can not parse the file" + e.getMessage());
                             }
+
                     }
 
                 }}catch(IOException e){
