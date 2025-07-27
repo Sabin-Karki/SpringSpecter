54ac32d7227358fc27980a147d10439e45e67689
sabin <sabinkarki064@gmail.com>
2025-07-27 18:50:01 +0545

    improved  output format

--- src/main/java/com/scanner/Scanner/ConsoleReporter.java
+++ src/main/java/com/scanner/Scanner/ConsoleReporter.java
@@ -7,9 +7,12 @@ import java.util.List;
 //only job is to print report
 @Component
 public class ConsoleReporter {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public void printReport(Path file, List<String> vulnerabilities){
       // loop through vulnerability list and print the vulnerability
-            System.out.println("\u001B[31m" + " [FAIL] VULNERABILITIES DETECTED IN: " + file.getFileName() + "\\u001B[0m"  );
+            System.out.println(ANSI_RED + " [FAIL] VULNERABILITIES DETECTED IN: " + file.getFileName() + ANSI_RESET  );
      for ( String vulnerability : vulnerabilities){
          System.err.println(" - " + vulnerability);
      }
@@ -17,7 +20,7 @@ public class ConsoleReporter {
     }
     public void printPass(Path file , String message){
             // Print the green PASS header.
-            System.out.println("\u001B[32m" + "[PASS] " + message + ": " + file.getFileName() + "\u001B[0m");
+            System.out.println(ANSI_GREEN + "[PASS] " + message + ": " + file.getFileName() + ANSI_RESET);
         }
     }
 
